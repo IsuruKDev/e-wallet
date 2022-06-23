4a6d96e3d1288233ae217a6669944770517cf15c
Isuru Kularathna <isurujr.online@gmail.com>
2022-06-23 12:27:57 +0530

    create a server-side e-wallet app

---
+++ src/test/java/com/nagel/kuehne/ewallet/EWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagel.kuehne.ewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
